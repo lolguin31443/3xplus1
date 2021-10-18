e8b4a1c4d85a89cc96b252b94f11a5013720c766
Leo Olguin <lolguin@legendnetworking.com>
2021-10-18 16:22:16 -0500

    Add project files.

---
+++ 3xplus1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _3xplus1
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a number between 2 and Infinity");
+            int userinput;
+            userinput = Convert.ToInt32(Console.ReadLine());
+
+            userinput = userinput * 3;
+            userinput++;
+            Convert.ToString(userinput);
+            Console.WriteLine(userinput);
+            Convert.ToInt32(userinput);
+            while (userinput >= 1)
+            {
+                if (userinput % 2 == 0)
+                {
+                    userinput = userinput / 2;
+                    Convert.ToString(userinput);
+                    Console.WriteLine(userinput);
+                    Convert.ToInt32(userinput);
+                }
+                else
+                {
+                    userinput = userinput * 3;
+                    userinput++;
+                    Convert.ToString(userinput);
+                    Console.WriteLine(userinput);
+                    Convert.ToInt32(userinput);
+                }
+                if (userinput == 4 || userinput == 1)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
